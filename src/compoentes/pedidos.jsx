11fca44199c29cf5ce8478010d24db298e551d06
Marcela Acosta <74275975+estherAcosta@users.noreply.github.com>
2022-12-09 16:58:05 -0500

    RETOQUE FINAL

--- src/compoentes/pedidos.jsx
+++ src/compoentes/pedidos.jsx
@@ -4,6 +4,12 @@ const Pedidos = () => {
     return    (
           <div>
     <Link to="Pedido">Pedidos</Link>
+    <span> 🚕👮🚕👮</span>
+          <span>Gracias por adquirir nuestro Servicio</span>
+          <span> 🚕👮🚕👮</span>
+    <h3>Pago en linea </h3>
+    <ul></ul>
+    <h3>Pago en efectivo </h3>
      </div>
     )
   };
